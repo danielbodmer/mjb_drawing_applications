2bc6c6c80c47af2e60c98db6d6670fd2a243c719
Daniel Bodmer <danny.bodmer@gmail.com>
2020-10-30 11:29:48 +0000

    create timer.js file

--- timer/index.js
+++ timer/index.js
@@ -13,48 +13,18 @@
 - displays a timer
 - shows an animated border around the timer
 */
-
-class Timer {
-  /*
-  durationInput, startButton, pauseButton -> DOM elements passed into constructor
-  start() - start timer
-  pause() - pause timer
-  onDurationChange() - user changes timer length
-  tick() - every 1 second tick of the timer
-  */
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  // using arrow function solves issues of 'this' returning the button element instead of the class object
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-  pause = () => {
-    clearInterval(this.interval);
-  };
-  tick = () => {
-    if (this.timeRemaining <= 0) this.pause();
-    else this.timeRemaining = this.timeRemaining - 1;
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('timer started!');
+  },
+  onTick() {
+    console.log('timer ticked!');
+  },
+  onComplete() {
+    console.log('timer complete');
+  },
+});
---
+++ timer/timer.js
@@ -0,0 +1,51 @@
+class Timer {
+  /*
+  durationInput, startButton, pauseButton -> DOM elements passed into constructor
+  start() - start timer
+  pause() - pause timer
+  onDurationChange() - user changes timer length
+  tick() - every 1 second tick of the timer
+  */
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  // using arrow function solves issues of 'this' returning the button element instead of the class object
+  start = () => {
+    if (this.onStart) this.onStart();
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+  pause = () => {
+    clearInterval(this.interval);
+  };
+  tick = () => {
+    // this.timeRemaining <= 0 ? this.pause() : (this.timeRemaining -= 1);
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) this.onComplete();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) this.onTick();
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
